684f76eee24e880e19303e1a56b3280d1d3b695e
William Brown <willmb321@gmail.com>
2025-07-19 19:34:59 -0500

    Started testing and adding functionality to sonarSweep.

--- src/main.cpp
+++ src/main.cpp
@@ -1,36 +1,59 @@
+/*--------------------------------------------------------------------
+Name:   William Brown
+Date:   7/19/2025   
+File:   sensorLib.cpp
+
+Purp:  
+
+Doc:  
+--------------------------------------------------------------------*/
+
 #include <Arduino.h>
 #include "servoLib.h"
 #include "sensorLib.h"
 
 
 int range = 18;
-volatile int servo_position = 0;
 volatile int servo_direction = -1; // starts going left
 
-void setup() {
-  Serial.begin(9600);
-  Serial.print("Starting up...");
-  sei();
-}
-
-void loop() {
-  //Servo needs to conituously turn back and forth
-  //Sensor scans and checks for object
-    //Performs something if there is an object in range
-
-  //sonarSweep();
-}
-
+/**
+ * Servo needs to conituously turn back and forth
+ * Sensor scans and checks for object
+ *    Performs something if there is an object in range
+ * 
+**/
 void sonarSweep(){
   //Servo sweep logic
-  if ((servo_position == 8) || (servo_position == 38)){
+  if ((getOCR1B() == 8) || (getOCR1B() == 38)){
     servo_direction*= -1;
   }
   turnServo(servo_direction, 1);
 
+  /*
   //Sensor logic
   if(checkObject(range)){
     Serial.print("Object detected at "); Serial.print(sensorMeasure()); Serial.print(" in!");
     //Add display logic
   }
+*/
+}
+
+/**
+ * Temp function to test servo is working. 
+**/
+void servoTest(){
+  turnServo(-1,1);
+}
+
+void setup() {
+  Serial.begin(9600);
+  Serial.print("Starting up...");
+  turnSetServo(-1);
+  turnSetServo(0);
+  sei();
+}
+
+void loop() {
+  sonarSweep();
+
 }
